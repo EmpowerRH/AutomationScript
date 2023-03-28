2675d14dff6d0b9516c951531f39415ab8142618
Ryan Hepworth <ryan.hepworth@dazn.com>
2023-03-28 15:44:33 +0100

    order change

---
+++ Empower2_1/Properties/AssemblyInfo.cs
@@ -0,0 +1,16 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Empower2_1")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Skyline Communications")]
+[assembly: AssemblyProduct("Empower2_1")]
+[assembly: AssemblyCopyright("Copyright © Skyline Communications")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+[assembly: Guid("99DE3C5B-DBC8-47AF-A286-7F85A32A89DB")]
+[assembly: AssemblyVersion("1.0.0.0")]
